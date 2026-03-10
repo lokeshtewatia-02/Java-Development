3b03b5851e13ee0bf022d84e7f5d72a550cb4d32
Lokesh <lokeshtewatia003@gmail.com>
2026-03-10 23:00:54 +0530

    new

---
+++ Spring Framework/springcore/src/main/java/com/springcore/collection/test.java
@@ -0,0 +1,17 @@
+package com.springcore.collection;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class test {
+	
+	public static void main(String[]args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/collection/config1.xml");
+	   Emp emp1=(Emp) context.getBean("emp1");
+	   System.out.println(emp1.getName());
+	   System.out.println(emp1.getPhones());
+	   System.out.println(emp1.getAddresses());
+	   System.out.println(emp1.getCourses());
+	   
+	}
+
+}
